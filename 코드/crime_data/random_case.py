7ac6712cff28a76f8b3d5b01c50efb311b84e509
hhlab <96fbgudwn@naver.com>
2021-04-12 16:44:37 +0900

    Fix arranged files and codes

--- 코드/crime_data/random_case.py
+++ 코드/crime_data/random_case.py
@@ -1,8 +1,8 @@
-'''
-1. 좌표를 화면에 찍고
-2. '1' + 'q' 키를 누르면
-3. 색칠된 그리드 내에서만 범죄 데이터 생성
-'''
+
+# ================================================
+# random_case.py
+# - 랜덤하게 범죄 사례를 생성
+# ================================================
 
 import numpy as np
 import cv2
@@ -10,45 +10,23 @@ import random
 import openpyxl
 from const_data import *
 
+''' ↓ 저장할 엑셀 파일 이름 입력'''
+FILE_NAME = "crime_data"
+CRIME_TOTAL = [3731, 3454, 3084, 2684, 2530, 2827]
 
-# 아예 랜덤 + * arificial_data.py(3000) = 실제 건수
-# * 추가 데이터 3000건 =
-# 낮 and 높인 그리드에 범죄 1500건 :
-# 낮 xor 높인 그리드에 범죄 1500건 :
-
-# 시간(0) : CCTV-경찰서, 유동인구-거주인구
-#
-#
-# 시간(1) : CCTV-경찰서, 유동인구-거주인구
-#
-#
-# 시간(2) : CCTV-경찰서, 유동인구-거주인구
-#
-#
-# 시간(3) : CCTV-경찰서, 유동인구-거주인구
-#
-#
-
-''' ↓ 저장할 엑셀 파일 이름, 연도별 경향성 데이터 수 입력'''
-FILE_NAME = "crime_data_a2"
-CRIME_TOTAL = [3000, 3000, 3000, 3000, 3000, 3000]  # 2014년 - 2019년
-
-O_LATITUDE = 37.57244364
-O_LONGTITUDE = 126.96095890
-LAT_GAP = 0.000896694
-LONG_GAP = 0.001138112
 
 WIDTH = 59
 HEIGHT = 32
-
 CRIME_KEY = {0: '절도', 1: '폭행', 2: '살인', 3: '강간', 4: '강도'}
 YEAR_KEY = {0: '2014', 1: '2015', 2: '2016', 3: '2017', 4: '2018', 5: '2019'}
 
 
-
 # 그리드 이미지를 배열로 변환
-img = cv2.imread("grid.png", 0)
+img = cv2.imread("img/grid.png", 0)
 img = cv2.resize(dsize=(WIDTH, HEIGHT), src=img)
+
+
+# 배경이 아닌 그리드의 좌표만 배열에 따로 저장 [위도, 경도]
 grids = []
 result = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
 for x in range(WIDTH):
@@ -57,11 +35,10 @@ for x in range(WIDTH):
             grids.append([y, x])
 
 # 엑셀 파일 생성(연도별 시트 생성)
-wb = openpyxl.Workbook()
-grd_switch = False
 for year in range(6):
+    wb = openpyxl.Workbook()
     print(f'{YEAR_KEY[year]}...')
-    sheet = wb.create_sheet(f'{YEAR_KEY[year]}')
+    sheet = wb.active
     sheet['A1'] = "Type"
     sheet['B1'] = "Date-Time"
     sheet['C1'] = "Latitude"
@@ -105,26 +82,16 @@ for year in range(6):
         sheet.cell(row=i, column=2).value = date + " " + time
 
         # 범죄지점 : 그리드를 랜덤으로 선택하고, 그리드 내부에서 랜덤생성
-        tt = int(hour/6)
-        if grd_switch:
-            gg = DANGER_GRID_AND
-        else:
-            gg = DANGER_GRID_OR
-        grd_switch = (grd_switch == False)
-        ll = len(gg[tt])
-        ii = int(random.uniform(0, ll))
-        idx = gg[tt][ii]
+        idx = int(random.uniform(0, len(grids)))
         x, y = grids[idx]
         gx, gy = O_LATITUDE - x * LAT_GAP, O_LONGTITUDE + y * LONG_GAP
         sheet.cell(row=i, column=3).value = random.uniform(gx - LAT_GAP, gx)
         sheet.cell(row=i, column=4).value = random.uniform(gy, gy + LONG_GAP)
         sheet.cell(row=i, column=5).value = idx
-
+    wb.save(f'data\{FILE_NAME}_{YEAR_KEY[year]}.xlsx')
+    wb.close()
 
 # 파일 저장
-wb.remove(wb['Sheet'])
-wb.save(FILE_NAME + '.xlsx')
 print('Excel file created!')
-
 
 
