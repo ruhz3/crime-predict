0964f47a4af2f368f3d20ccdb675ea058ad59839
hhlab <96fbgudwn@naver.com>
2021-05-17 15:03:36 +0900

    commit before change

--- 코드/show_route.py
+++ 코드/show_route.py
@@ -11,32 +11,45 @@ section_dict = {
     1: [100, 200, 100],
     2: [100, 100, 200]
 }
+
+# 구역별로 색상을 다르게 매핑하기 위한 딕셔너리 생성
 color_dict_0 = {}
-for i in range(25):
-    color_dict_0[i] = [255, 255 - i * 8, 255 - i * 8]
 color_dict_1 = {}
-for i in range(25):
-    color_dict_1[i] = [255 - i * 8, 255, 255 - i * 8]
 color_dict_2 = {}
 for i in range(25):
+    color_dict_0[i] = [255, 255 - i * 8, 255 - i * 8]
+    color_dict_1[i] = [255 - i * 8, 255, 255 - i * 8]
     color_dict_2[i] = [255 - i * 8, 255 - i * 8, 255]
 
-
-img = cv2.imread("grid.png", 1)
+# 그리드 배열 불러오기
+img = cv2.imread("img/grid.png", 1)
 img = cv2.resize(dsize=(WIDTH, HEIGHT), src=img)
 result = np.full((HEIGHT, WIDTH, 3), 255, np.uint8)
 
-
-num2grid = []
+# 그리드 번호를 좌표로 매핑할 배열
+# : num2coord['그리드 번호'] = 좌표의 y, x값 반환
+num2coord = []
 for x in range(WIDTH):
     for y in range(HEIGHT):
         if img[y][x][0] != 255 or img[y][x][1] != 255 or img[y][x][2] != 255:
-            num2grid.append([y, x])
+            num2coord.append([y, x])
 
+# 좌표 번호를 그리드 좌표로 반환
+# : coord2num['y 좌표']['x 좌표'] = 그리드 번호 반환
+# : 해당 좌표가 배경이라면 -1 반환
+coord2num = np.full((HEIGHT, WIDTH), -1)
+idx = 0
+for x in range(WIDTH):
+    for y in range(HEIGHT):
+        if img[y][x][0] != 255 or img[y][x][1] != 255 or img[y][x][2] != 255:
+            coord2num[y][x] = idx
+            idx += 1
+
+# 경유지의 좌표를 색상 순서대로 매핑
 for section in range(3):
     key = 0
     for num in ROUTE[section]:
-        y, x = num2grid[num]
+        y, x = num2coord[num]
         if section == 0:
             img[y][x][0] = color_dict_0[key][2]
             img[y][x][1] = color_dict_0[key][1]
@@ -51,6 +64,7 @@ for section in range(3):
             img[y][x][2] = color_dict_2[key][0]
         key += 1
 
+# 시각화
 res = cv2.resize(img, dsize=(WIDTH*20, HEIGHT*20), interpolation=cv2.INTER_NEAREST_EXACT)
 cv2.imshow('res', res)
 cv2.waitKey()
