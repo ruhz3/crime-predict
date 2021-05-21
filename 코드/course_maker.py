5cbc0bd486842e149fc808e7732c69a196ae647b
Young-37 <67675422+Young-37@users.noreply.github.com>
2021-05-21 17:23:35 +0900

    [Young] fix bug, add remark

--- 코드/course_maker.py
+++ 코드/course_maker.py
@@ -49,8 +49,8 @@ def idx2coor(idx_result):
         gx, gy = O_LATITUDE - x * LAT_GAP, O_LONGTITUDE + y * LONG_GAP
         string = str(gy) + "," + str(gx)
         temp.append(string)
-    coor_result.append(temp)
-    return coor_result
+
+    return temp
 
 
 """ Travelling Salesman / Greedy 주석 풀어 바로 사용 """
@@ -59,40 +59,9 @@ def create_data_model(distance_matrix):
     """Stores the data for the problem."""
     data = {}
     data['distance_matrix'] = distance_matrix
-    '''[
-        [0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972],
-        [2451, 0, 1745, 1524, 831, 1240, 959, 2596, 403, 1589, 1374, 357, 579],
-        [713, 1745, 0, 355, 920, 803, 1737, 851, 1858, 262, 940, 1453, 1260],
-        [1018, 1524, 355, 0, 700, 862, 1395, 1123, 1584, 466, 1056, 1280, 987],
-        [1631, 831, 920, 700, 0, 663, 1021, 1769, 949, 796, 879, 586, 371],
-        [1374, 1240, 803, 862, 663, 0, 1681, 1551, 1765, 547, 225, 887, 999],
-        [2408, 959, 1737, 1395, 1021, 1681, 0, 2493, 678, 1724, 1891, 1114, 701],
-        [213, 2596, 851, 1123, 1769, 1551, 2493, 0, 2699, 1038, 1605, 2300, 2099],
-        [2571, 403, 1858, 1584, 949, 1765, 678, 2699, 0, 1744, 1645, 653, 600],
-        [875, 1589, 262, 466, 796, 547, 1724, 1038, 1744, 0, 679, 1272, 1162],
-        [1420, 1374, 940, 1056, 879, 225, 1891, 1605, 1645, 679, 0, 1017, 1200],
-        [2145, 357, 1453, 1280, 586, 887, 1114, 2300, 653, 1272, 1017, 0, 504],
-        [1972, 579, 1260, 987, 371, 999, 701, 2099, 600, 1162, 1200, 504, 0],
-    ] ''' # yapf: disable
     data['num_vehicles'] = 1
     data['depot'] = 0
     return data
-
-
-def print_solution(manager, routing, solution):
-    """Prints solution on console."""
-    print('Objective: {} miles'.format(solution.ObjectiveValue()))
-    index = routing.Start(0)
-    plan_output = 'Route for vehicle 0:\n'
-    route_distance = 0
-    while not routing.IsEnd(index):
-        plan_output += ' {} ->'.format(manager.IndexToNode(index))
-        previous_index = index
-        index = solution.Value(routing.NextVar(index))
-        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
-    plan_output += ' {}\n'.format(manager.IndexToNode(index))
-    print(plan_output)
-    plan_output += 'Route distance: {}miles\n'.format(route_distance)
 
 
 def get_routes(solution, routing, manager):
@@ -117,6 +86,7 @@ def main():
     # 히트맵에서 thresholding 한 결과
     selected_grids = extract_points(ALGORITHM, POINTS)
 
+    # 구역별로 알고리즘 실행
     for index in range(len(selected_grids)):
         # distance matrix 생성
         distance_matrix = make_distance_matrix(selected_grids[index])
@@ -124,6 +94,10 @@ def main():
         data = create_data_model(distance_matrix)
 
         # Create the routing index manager.
+        # RoutingIndexManager input
+        # distance matrix 행 개수
+        # vehicle 개수 (TSP의 경우 차량은 1대)
+        # depot인 노드
         manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
                                                data['num_vehicles'], data['depot'])
 
@@ -150,24 +124,26 @@ def main():
         # Solve the problem.
         solution = routing.SolveWithParameters(search_parameters)
 
-        # Print solution on console.
-        # if solution:
-        #    print_solution(manager, routing, solution)
-
         routes = get_routes(solution, routing, manager)
         # Display the routes.
         # for i, route in enumerate(routes):
         #    print('Route', i, route)
 
         idx_temp = []
+        coor_temp = []
+
+        # index result
         for j in range(len(routes[0]) - 1):
             idx_temp.append(selected_grids[index][routes[0][j]])
         idx_result.append(idx_temp)
+
+        # coordinate result
         coor_temp = idx2coor(idx_temp)
         coor_result.append(coor_temp)
 
     print(idx_result)
     print(coor_result)
+
 # </editor-fold>
 # <editor-fold desc="Greedy Algorithm">
 '''
